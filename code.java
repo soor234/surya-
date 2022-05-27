3fde5e2be23a6f2d677aa402776e0395a2f16929
surya narayanan <74855435+soor234@users.noreply.github.com>
2022-05-27 12:37:48 +0530

    code updated

---
+++ code.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public static void main(string  args[])
+{
+ public class  string
+ {
+     int t;
+     scanner scn= new scanner(scn);
+     int sc = new  sc(int)
+     while(sc.readline())
+     {
+         int n,k;
+         {
+ buffered  reader = new  buffered reader(sc);
+ {
+     s.nextline();
+     {
+         while(s.parseline())
+         {
+             integer parse int= new integer parse int(sc)
+             {
+                 int.sc formatline(n,k)
+                 {
+                     integer.parse line()
+                     {
+                         
+                     }
+                 }
+             }
+         }
+     }
+ }
+         }
+     }
+     
+ }
+}
